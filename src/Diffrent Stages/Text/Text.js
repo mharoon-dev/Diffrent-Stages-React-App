d89fdb82e664b97cb09c5d5088dd99ce3642d3d9
mharoon-dev <haroon.aghani@gmail.com>
2024-02-21 21:33:10 +0500

    changes and done

--- src/Diffrent Stages/Text/Text.js
+++ src/Diffrent Stages/Text/Text.js
@@ -1,21 +1,20 @@
-export default function Text({text}) {
-    const texts = [
-        "Step 1: Learn JavaScript",
-        "Step 2: Learn React",
-        "Step 3: Invest Your new Income",
-    ]
-    return (
-        <div className="col-12 w-75 d-flex flex-column justify-content-around align-items-center">
-        <h3
-          key={text} 
-          style={{
-            marginBottom: "0",
-            color: "black",
-          }}
-        >
-          {text}
-        </h3>
-
-        </div>
-    )
+export default function Text({ text }) {
+  const texts = [
+    "Step 1: Learn JavaScript",
+    "Step 2: Learn React",
+    "Step 3: Invest Your new Income",
+  ];
+  return (
+    <div className="col-12 w-75 d-flex flex-column justify-content-around align-items-center">
+      <h3
+        key={text}
+        style={{
+          marginBottom: "0",
+          color: "black",
+        }}
+      >
+        {text}
+      </h3>
+    </div>
+  );
 }
